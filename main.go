c371c0e5fff1da651688adfae5aa159508eedc39
RobotHuang <1183598761@qq.com>
2021-04-24 11:07:52 +0800

    feat: Init RedisManager and RadosManager
    Signed-off-by: RobotHuang <1183598761@qq.com>

--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"octopus/cache"
 	"octopus/connection"
 	"octopus/router"
 )
 
-var (
-	cacheFlag bool
-)
+//var (
+//	cacheFlag bool
+//)
 
 func main() {
-	cacheFlag = *(flag.Bool("cache", false, "Cache enable or not"))
+	//cacheFlag = *(flag.Bool("cache", false, "Cache enable or not"))
 	var re = connection.NewRedis("tcp", "127.0.0.1:6379", "")
 	var err = re.Init()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	connection.InitRedisManager(re)
 	r, err := connection.NewRados()
 	if err != nil {
 		fmt.Println(err)
@@ -30,10 +29,11 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	if cacheFlag == true {
-		c := cache.NewLRUCache(10, 10)
-		cache.InitCache(c)
-	}
+	connection.InitRadosManager(r)
+	//if cacheFlag == true {
+	//	c := cache.NewLRUCache(10, 10)
+	//	cache.InitCache(c)
+	//}
 	var g *gin.Engine
 	g = router.SetRouter()
 	if err := g.Run(":8080"); err != nil {
